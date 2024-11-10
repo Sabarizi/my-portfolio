370b3fe27e3c5d0195718203db42114616792ad7
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-11-10 15:06:47 +0500

    add

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -17,11 +17,11 @@ export default function About() {
                 <div className={styles.aboutText}>
                     <h1 className={styles.aboutHeading}>About Me</h1>
                     <p className={styles.aboutParagraph}>
-                        "As a dedicated and aspiring web developer, I am passionate about building functional, visually appealing, 
+                        As a dedicated and aspiring web developer, I am passionate about building functional, visually appealing, 
                         and user-friendly websites. With a strong foundation in HTML, CSS, JavaScript, and frameworks like React and Next.js, 
                         I bring both creativity and technical skills to my projects. I love tackling complex problems, constantly improving my skills, 
                         and staying updated with the latest trends in web development. My journey is driven by a love for learning, curiosity, 
-                        and a desire to craft intuitive, dynamic solutions that resonate with users."
+                        and a desire to craft intuitive, dynamic solutions that resonate with users.
                     </p>
                 </div>
             </div>
